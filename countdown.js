62b0f4ad780ab719c4be19ef6bebbc6dbe792d09
isouffrant1 <60717996+isouffrant1@users.noreply.github.com>
2022-12-02 01:34:06 -0500

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+const birthday = '28 March 2023';
+
+function countdown() {
+    const birthdayDate = new Date(birthday);
+    const currentDate = new Date();
+ 
+    const totalSeconds = (birthdayDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+countdown();
+
+setInterval(countdown, 1000)
